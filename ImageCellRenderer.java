cd22b00c016233a3d06c751e9a569fb4abeba3e9
Darwin <93103712+darwin712@users.noreply.github.com>
2025-05-14 19:19:31 -0700

    tamaño fijo para imagenes

--- ImageCellRenderer.java
+++ ImageCellRenderer.java
@@ -6,8 +6,14 @@ public class ImageCellRenderer extends DefaultTableCellRenderer {
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         if (value instanceof ImageIcon) {
-            JLabel label = new JLabel((ImageIcon) value);
-            label.setHorizontalAlignment(JLabel.CENTER); // Centra la imagen en la celda
+            ImageIcon originalIcon = (ImageIcon) value;
+
+            //Escalar imagen a 100x100
+            Image scaledImage = originalIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+            ImageIcon scaledIcon = new ImageIcon(scaledImage);
+
+            JLabel label = new JLabel(scaledIcon);
+            label.setHorizontalAlignment(JLabel.CENTER); // Centrar la imagen en la celda
             return label;
         }
         return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
