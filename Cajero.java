3cd41d3f15bbaae07ddceded58d607f23f12de47
Darwin <93103712+darwin712@users.noreply.github.com>
2025-05-14 18:59:51 -0700

    ventana de cajero en proceso

---
+++ Cajero.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Cajero extends JPanel{
+    public Cajero(){
+        setBackground(Color.decode("#735238"));
+        setSize(950, 560);
+        setLayout(new BorderLayout());
+    }
+}
