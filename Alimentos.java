ab13341a588955c3d594474f2a9a6119fb549311
Darwin <93103712+darwin712@users.noreply.github.com>
2025-05-16 19:53:56 -0700

    arreglos

--- Alimentos.java
+++ Alimentos.java
@@ -1,9 +1,10 @@
 
-public class Alimentos extends Productos  {
+public class Alimentos extends Productos {
 
 
-    public Alimentos(String id, String nombre,String medidas,String descripcion, String rutaImagen, String cantidad, String existencias) {
-        super(id, nombre, descripcion, rutaImagen,medidas, cantidad, existencias);
+    public Alimentos(String id, String nombre, String descripcion, String rutaImagen, String cantidad, String existencias, String medidas) {
+        super(id, nombre, descripcion, rutaImagen, cantidad,medidas, existencias);
        
     }
-}
+    
+    }
